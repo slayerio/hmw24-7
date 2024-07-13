ab045eb7b23c4757e6d38f13762ee257434f39c0
slayerio <danielelish33@gmail.com>
2024-07-13 18:37:27 +0300

    Add files via upload

---
+++ hmw2.py
@@ -0,0 +1,41 @@
+#hmw 2 answers
+
+x = float(input("select 3 random numbers"));
+z = float(input("great"));
+y = float(input("last one"));
+
+
+print(f"the sum of the numbers are {x + y + z} and the product is {x * y * z}");
+
+
+
+#2.
+x = float(input("select 2 random numbers"));
+y = float(input("another one"));
+
+
+print(f"the remain of the numbers are {x - y}");
+
+#3.
+x = input("give me a word");
+y = input("give me a word");
+print(f"*{x}*{y}* \n-{x}-{y}-")
+
+#4.
+x = int(input("select a number"));
+y = int(input("select another number"));
+
+
+if x <= y:
+   print(f"{x} \n{y}");
+else:
+   print(f"{y} \n{x}");
+
+x = int(input("select a number"));
+y = int(input("select another number"));
+
+
+if x <= y:
+   print(y);
+else:
+   print(x);
